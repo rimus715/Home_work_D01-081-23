9b1f85246407fd6d6378c42358ebb23f8f5f2b13
rimus715 <166112017+rimus715@users.noreply.github.com>
2024-09-13 18:19:50 +0300

    Add files via upload

---
+++ HW1/tree.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+    printf("    *\n");
+    printf("   ***\n");
+    printf("  *****\n");
+    printf(" *******\n");
+    printf("  HHOHH\n");
+    printf("  ZZZZZ\n");
+    return 0;
+}
